fec660babd16a4244b7462d409eb2b2e26eb69dd
khadar shaik <mskkhadar@gmail.com>
2018-09-05 15:21:46 +0530

    recaptcha using google api

---
+++ recaptcha/src/org/recaptcha/controller/LoginServlet.java
@@ -0,0 +1,46 @@
+package org.recaptcha.controller;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.recaptcha.utils.VerifyRecaptcha;
+/**
+ * Servlet implementation class LoginServlet
+ */
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = -6506682026701304964L;
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		//hardcoded user name and password
+		String userID = "user" ;
+		String password = "password";
+		// get request parameters for userID and password
+		String user = request.getParameter("user");
+		String pwd = request.getParameter("pwd");
+		// get reCAPTCHA request param
+		String gRecaptchaResponse = request
+				.getParameter("g-recaptcha-response");
+		System.out.println(gRecaptchaResponse);
+		boolean verify = VerifyRecaptcha.verify(gRecaptchaResponse);
+		// get servlet config init params
+		// logging example
+		System.out.println("User=" + user + "::password=" + pwd + "::Captcha Verify"+verify);
+		if (userID.equals(user) && password.equals(pwd) && verify) {
+			response.sendRedirect("LoginSuccess.jsp");
+		} else {
+			RequestDispatcher rd = getServletContext().getRequestDispatcher(
+					"/login.html");
+			PrintWriter out = response.getWriter();
+			if (verify) {
+				out.println("<font color=red>Either user name or password is wrong.</font>");
+			} else {
+				out.println("<font color=red>You missed the Captcha.</font>");
+			}
+			rd.include(request, response);
+		}
+	}
+}
